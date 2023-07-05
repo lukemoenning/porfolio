161b5ce79ebf85c7aeaaf6720f3fc564e7e20df7
lukemoenning <luke.moenning@gmail.com>
2023-07-04 21:18:36 -0500

    resume page

--- pages/resume/index.tsx
+++ pages/resume/index.tsx
@@ -1,8 +1,54 @@
+/**
+ * Resume component
+ */
+
 import React from 'react'
+import styled from 'styled-components'
+import { 
+  BodyWrapper,
+  BodyHeaderWrapper,
+  HeaderTitle,
+  HeaderDescription,
+  BodyHeaderNarrow, 
+  BodyContentWrapper, 
+  BodyContentNarrow
+} from '@/app/libs/common-components'
+import { theme } from '@/app/libs/theme'
+
+
+const Pdf = styled.iframe`
+  margin: 25px 0 25px 0;
+  top: 0;
+  left: 0;
+  width: 100%;
+  min-height: 800px;
+`
+
+const NewTabPDF = styled.a`
+  text-decoration: none;
+  color: ${theme.colors.lightBlue};
+`
 
 function Resume() {
   return (
-    <div>Resume</div>
+    <BodyWrapper>
+      <BodyHeaderWrapper>
+        <BodyHeaderNarrow>
+          <HeaderTitle>Resume.</HeaderTitle>
+          <HeaderDescription>
+            An official copy of my resume. <NewTabPDF href={'/Moenning_Luke_Resume.pdf'} target='blank'>
+              Open in a new tab.
+            </NewTabPDF>
+          </HeaderDescription>
+        </BodyHeaderNarrow>
+      </BodyHeaderWrapper>
+
+      <BodyContentWrapper>
+        <BodyContentNarrow>
+          <Pdf src={'/Moenning_Luke_Resume.pdf'} />
+        </BodyContentNarrow>
+      </BodyContentWrapper>
+    </BodyWrapper>
   )
 }
 
